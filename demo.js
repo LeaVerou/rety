468c0672f2e666072be20e4dac60953baa89ecd7
Lea Verou <lea@verou.me>
2022-01-14 14:50:36 +0200

    Turn Rety demo into a reusable custom element

--- demo.js
+++ demo.js
@@ -1,65 +1,221 @@
 import Recorder from "./src/recorder.js";
 import Replayer from "./src/replayer.js";
 
-let initialCode = {
-	css: `* {
-	color: red;
-}`,
-	html: `<button>Click me</button>
-<button class="pink">Click me</button>`
-};
-
-sourceCSS.textContent = initialCode.css;
-destCSS.textContent = initialCode.css;
-sourceHTML.textContent = initialCode.html;
-destHTML.textContent = initialCode.html;
-
-for (let t of $$("#demo-textareas textarea")) {
-	t.value = t.textContent;
-	t.dispatchEvent(new InputEvent('input'));
+function $$(selector, context = document) {
+	return Array.from(context.querySelectorAll(selector));
 }
 
-window.recorder = new Recorder({css: sourceCSS, html: sourceHTML});
-window.replayer = new Replayer({css: destCSS, html: destHTML});
+function $(s, c = document) {
+	return c.querySelector(s);
+}
 
 function timeout(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-setTimeout(recorder.start(), 1000);
+class RetyDemo extends HTMLElement {
+	constructor () {
+		super();
 
-let lastAction;
-recorder.addEventListener("actionschange", async function (evt) {
-	await lastAction;
-	let action = evt.detail.action;
+		this.dom = {};
+		this.withSource = this.classList.contains("with-source");
 
-	log.value = stringifyArray(recorder.actions);
-	log.dispatchEvent(new InputEvent('input'));
+		this.dest = Object.fromEntries($$("textarea:not(.language-actions-json)", this).map(el => [Prism.util.getLanguage(el), el]));
 
-	lastAction = replayer.run(action);
-});
+		this.dom.editorWrapper = document.createElement("div");
+		this.dom.editorWrapper.classList.add("demo-textareas");
+		this.appendChild(this.dom.editorWrapper);
 
-replayer.addEventListener("play", async evt => {
-	let played = replayer.played;
-	let index = played.length;
-	let action = evt.detail.action;
+		if (this.withSource) {
+			this.sources = {}
+			for (let lang in this.dest) {
+				let editor = this.dest[lang];
+				let source = editor.cloneNode(true);
+				editor.classList.add("dest");
+				source.classList.add("source");
+				this.sources[lang] = source;
+			}
+
+			this.dom.editorWrapper.innerHTML = `
+			<label class="source-label">
+				Make some edits here:
+			</label>
+			<label class="dest-label">
+				Observe them being replayed here:
+			</label>`;
+
+			$(".source-label", this.dom.editorWrapper).append(...Object.values(this.sources));
+
+			this.recorder = new Recorder(this.sources);
+			setTimeout(() => this.recorder.start(), 1000);
+		}
+		else {
+			this.dom.editorWrapper.innerHTML = `<label class="dest-label">
+		</label>`;
+		}
+
+		$(".dest-label", this.dom.editorWrapper).append(...Object.values(this.dest));
+
+		for (let t of $$("textarea", this)) {
+			t.classList.add("prism-live");
+			t.value = t.textContent;
+			t.dispatchEvent(new InputEvent('input'));
+		}
+
+		this.replayer = new Replayer(this.dest);
+
+		this.insertAdjacentHTML("beforeend", `<div class="controls">
+		<button onclick="this.closest('rety-demo').reset()">⏏️ Reset</button>
+		<button onclick="this.closest('rety-demo').rewind()">⏮ Rewind</button>
+		<button onclick="this.closest('rety-demo').next()">▶️ Play next</button>
+		<button onclick="this.closest('rety-demo').runUntilNextPause()">▶️ Play section</button>
+		<button onclick="this.closest('rety-demo').runAll()">▶️ Play from beginning</button>
+		<button onclick="this.closest('rety-demo').replayer.pause()">⏸ Pause</button>
+		<button onclick="this.closest('rety-demo').replayer.resume()">▶️ Resume</button>
+	</div>
+	<p>Actions log:</p>
+	<div class="log-container">
+
+	</div>`);
+
+		this.dom.log_container = $(".log-container", this);
+		this.log = $("textarea.language-actions-json", this);
+
+		if (this.log) {
+			// Existing actions log
+			this.log.classList.add("log", "prism-live");
+			this.dom.log_container.append(this.log);
+		}
+		else {
+			this.dom.log_container.innerHTML = `<textarea class="log language-actions-json prism-live"></textarea>`;
+			this.log = $(".log", this.dom.log_container);
+		}
+
+		if (this.recorder) {
+			let lastAction;
+			this.recorder.addEventListener("actionschange", async evt => {
+				await lastAction;
+				let action = evt.detail.action;
+
+				this.log.value = stringifyArray(this.recorder.actions);
+				this.log.dispatchEvent(new InputEvent('input'));
+
+				lastAction = this.replayer.run(action);
+			});
+		}
+
+		this.replayer.addEventListener("play", async evt => {
+			let played = this.replayer.played;
+			let index = played.length;
+			let action = evt.detail.action;
 
 
-	let actionTokens = log.parentNode.querySelectorAll(".token.action");
+			let actionTokens = this.log.parentNode.querySelectorAll(".token.action");
 
-	for (let i = 0; i < index; i++) {
-		let isLast = i === index - 1;
-		actionTokens[i].classList.toggle("current", isLast);
-		actionTokens[i].classList.toggle("played", !isLast);
+			for (let i = 0; i < index; i++) {
+				let isLast = i === index - 1;
+				actionTokens[i].classList.toggle("current", isLast);
+				actionTokens[i].classList.toggle("played", !isLast);
+			}
+		});
+
+		this.log.addEventListener("input", evt => this.#updateLogActions());
+
+		if (this.log.value) {
+			this.#updateLogActions();
+		}
 	}
-});
 
-log.addEventListener("input", evt => {
-	try {
-		window.logActions = JSON.parse(evt.target.value);
+	#updateLogActions () {
+		try {
+			this.logActions = JSON.parse(this.log.value);
+		}
+		catch (e) {}
 	}
-	catch (e) {}
-});
+
+	reset () {
+		this.recorder.actions = [];
+		this.replayer.stop();
+
+		for (let t of $$("textarea.dest", this)) {
+			t.value = t.textContent;
+			t.dispatchEvent(new InputEvent('input'));
+		}
+	}
+
+	rewind () {
+		this.replayer.played = [];
+		this.replayer.queue = this.logActions;
+
+		for (let t of $$("textarea.dest", this)) {
+			t.value = t.textContent;
+			t.dispatchEvent(new InputEvent('input'));
+		}
+	}
+
+	async next () {
+		let justEnded;
+
+		if (!this.replayer.queue || this.replayer.queue.length === 0) {
+			if (this.replayer.played?.length > 0) {
+				justEnded = true;
+				this.cleanup();
+			}
+			else {
+				this.prepare();
+			}
+
+			this.rewind();
+		}
+
+		if (!justEnded) {
+			await this.replayer.next();
+		}
+	}
+
+	async runUntilNextPause () {
+		this.replayer.editor.focus();
+
+		this.dom.log_container.classList.add("playing");
+
+		this.replayer.options.pauses = "pause";
+
+		if (this.replayer.queue) {
+			await this.replayer.resume();
+		}
+		else {
+			await this.runAll({pauses: "pause"})
+		}
+
+		this.dom.log_container.classList.remove("playing");
+	}
+
+	async runAll ({pauses = "delay"} = {}) {
+		this.replayer.editor.focus();
+
+		this.prepare();
+
+		this.replayer.options.pauses = pauses;
+
+		this.rewind();
+
+		await this.replayer.runAll(this.logActions);
+
+		await timeout(1000);
+
+		this.cleanup();
+	}
+
+	prepare () {
+		this.dom.log_container.classList.add("playing");
+	}
+
+	cleanup () {
+		this.dom.log_container.classList.remove("playing");
+		$$(".action:is(.played, .current)").forEach(e => e.classList.remove("played", "current"));
+	}
+}
+
 
 function stringifyArray(arr) {
 	let ret = "[\n\t";
@@ -79,97 +235,7 @@ function stringifyArray(arr) {
 	return ret + "]";
 }
 
-function $$(selector, context = document) {
-	return Array.from(context.querySelectorAll(selector));
-}
-
-function $(s, c = document) {
-	return c.querySelector(s);
-}
-
-window.Demo = {
-	reset () {
-		recorder.actions = [];
-		replayer.stop();
-
-		for (let t of $$("#demo-textareas textarea.dest")) {
-			t.value = t.textContent;
-			t.dispatchEvent(new InputEvent('input'));
-		}
-	},
-
-	rewind () {
-		replayer.played = [];
-		replayer.queue = logActions;
-
-		for (let t of $$("#demo-textareas textarea.dest")) {
-			t.value = t.textContent;
-			t.dispatchEvent(new InputEvent('input'));
-		}
-	},
-
-	async next () {
-		let justEnded;
-
-		if (!replayer.queue || replayer.queue.length === 0) {
-			if (replayer.played?.length > 0) {
-				justEnded = true;
-				Demo.cleanup();
-			}
-			else {
-				Demo.prepare();
-			}
-
-			Demo.rewind();
-		}
-
-		if (!justEnded) {
-			await replayer.next();
-		}
-	},
-
-	async runUntilNextPause () {
-		destCSS.focus();
-
-		log_container.classList.add("playing");
-
-		replayer.options.pauses = "pause";
-
-		if (replayer.queue) {
-			await replayer.resume();
-		}
-		else {
-			await Demo.runAll({pauses: "pause"})
-		}
-
-		log_container.classList.remove("playing");
-	},
-
-	async runAll ({pauses = "delay"} = {}) {
-		destCSS.focus();
-
-		Demo.prepare();
-
-		replayer.options.pauses = pauses;
-
-		Demo.rewind();
-
-		await replayer.runAll(logActions);
-
-		await timeout(1000);
-
-		Demo.cleanup();
-	},
-
-	prepare () {
-		log_container.classList.add("playing");
-	},
-
-	cleanup () {
-		log_container.classList.remove("playing");
-		$$(".action:is(.played, .current)").forEach(e => e.classList.remove("played", "current"));
-	}
-}
+customElements.define("rety-demo", RetyDemo);
 
 Prism.languages["actions-json"] = {
 	"action-delete": /\{"type":"delete.+?\}/gm,
